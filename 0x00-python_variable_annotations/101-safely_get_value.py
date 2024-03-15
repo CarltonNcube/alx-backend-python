64d653356c6bd34a31db5583c2c6439e92945374
CarltonNcube <carltonncube@outlook.com>
2024-03-15 21:22:58 +0000

    initial implementation of Python variable annotations exercises

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,26 +3,13 @@
 This module defines a function that safely retrieves a value from a dictionary.
 """
 
-from typing import Dict, TypeVar, Optional
+from typing import Any, Mapping, TypeVar, Union
+T = TypeVar('T')
 
-# Define type variables
-K = TypeVar('K')  # Type of keys in the dictionary
-V = TypeVar('V')  # Type of values in the dictionary
 
-def safely_get_value(dct: Dict[K, V], key: K, default: Optional[V] = None) -> V:
-    """
-    Safely retrieve a value from a dictionary.
-
-    Args:
-        dct (Dict[K, V]): The input dictionary.
-        key (K): The key to retrieve the value for.
-        default (Optional[V]): The default value to return if the key is not found.
-        Defaults to None.
-
-    Returns:
-        V: The value associated with the key in the dictionary, or the 
-        default value if the key is not found.
-    """
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]
+                     = None) -> Union[Any, T]:
+    """More type annotations"""
     if key in dct:
         return dct[key]
     else:
