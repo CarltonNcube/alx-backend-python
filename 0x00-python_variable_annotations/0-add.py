4b97c14695d1bc11cb64b5e7b9b0e739baaf87f1
CarltonNcube <carltonncube@outlook.com>
2024-03-15 20:57:34 +0000

    initial implementation of Python variable annotations exercises

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -4,14 +4,4 @@ This module defines a function that adds two floats.
 """
 
 def add(a: float, b: float) -> float:
-    """
-    Add two floats.
-
-    Args:
-        a (float): The first number.
-        b (float): The second number.
-
-    Returns:
-        float: The sum of a and b.
-    """
     return a + b
