82de56b97b05499a5405557b76954b65d677bd30
CarltonNcube <carltonncube@outlook.com>
2024-03-15 21:54:33 +0000

    initial implementation of Python variable annotations exercises

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-"""
-This module defines a function that calculates the length 
-of each element in a list.
-"""
+"""Defines a function to calculate the length of each element in a list."""
 from typing import Iterable, List, Sequence, Tuple
 
 
