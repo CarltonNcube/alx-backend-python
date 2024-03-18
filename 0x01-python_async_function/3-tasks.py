999f14f16383bc0b2c4a8c1afefa79bd864544b0
CarltonNcube <carltonncube@outlook.com>
2024-03-18 21:52:36 +0000

    Measures the total execution time for wait_n(n, max_delay)

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 """Creates an asyncio.Task object for waiting for a random delay."""
 import asyncio
-from typing import Task
-from random import uniform
-from basic_async_syntax import wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Task:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
-    Creates an asyncio.Task object for waiting for a random delay.
-    
+    Returns an asyncio.Task object for waiting for a random delay.
+
     Args:
         max_delay (int): Maximum delay in seconds.
 
