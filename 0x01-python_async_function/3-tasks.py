6fa981aaa6fe3d606e4edc921b4b125fa7d66d0b
CarltonNcube <carltonncube@outlook.com>
2024-03-18 20:51:57 +0000

    Creates an asyncio.Task object for waiting for a random delay

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """Creates an asyncio.Task object for waiting for a random delay."""
-
 import asyncio
 from typing import Task
+from random import uniform
 from basic_async_syntax import wait_random
+
 
 def task_wait_random(max_delay: int) -> Task:
     """
-    Returns an asyncio.Task object for waiting for a random delay.
+    Creates an asyncio.Task object for waiting for a random delay.
     
     Args:
         max_delay (int): Maximum delay in seconds.
@@ -16,10 +17,3 @@ def task_wait_random(max_delay: int) -> Task:
         asyncio.Task: Task object representing the asynchronous operation.
     """
     return asyncio.create_task(wait_random(max_delay))
-
-async def test(max_delay: int) -> float:
-    task = task_wait_random(max_delay)
-    await task
-    print(task.__class__)
-
-asyncio.run(test(5))
