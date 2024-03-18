64999ac907ee90e5a60c8465a18dc44a071a9da4
CarltonNcube <carltonncube@outlook.com>
2024-03-18 20:33:21 +0000

    Execute multiple coroutines at the same time with async

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""Importing the asyncio module for asynchronous programming"""
+"""Execute multiple coroutines at the same time with async"""
+from typing import List
 import asyncio
 import random
-
 
 async def wait_random(max_delay: int = 10) -> float:
     """
@@ -17,3 +17,11 @@ async def wait_random(max_delay: int = 10) -> float:
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawns wait_random n times with the specified max_delay
+        and returns the list of all the delays (float values)."""
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    completed_tasks = await asyncio.gather(*tasks)
+    return sorted(completed_tasks)
