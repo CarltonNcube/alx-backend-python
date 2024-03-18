8f392ca668b8057dcec28f0043fe483f52bc4034
CarltonNcube <carltonncube@outlook.com>
2024-03-18 20:23:29 +0000

    Importing the asyncio module for asynchronous programming

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
-'''
-Implementation of the wait_n coroutine
-'''
+"""Execute multiple coroutines at the same time with async"""
+from typing import List
 import asyncio
-import random
+from random import uniform
 
-async def wait_n(n, max_delay):
+
+async def wait_random(max_delay: int = 10) -> float:
     """
-    Spawns wait_random n times with the specified max_delay.
-    Returns the list of all the delays (float values) in ascending order.
+    Asynchronous coroutine that waits for a random delay between 0.
+
+    Args:
+        max_delay (int): Maximum delay in seconds (default is 10).
+
+    Returns:
+        float: The random delay.
     """
+    delay = uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawns wait_random n times with the specified max_delay
+        and returns the list of all the delays (float values)."""
     tasks = [wait_random(max_delay) for _ in range(n)]
     completed_tasks = await asyncio.gather(*tasks)
     return sorted(completed_tasks)
-
-async def wait_random(max_delay: int) -> float:
-    """Wait for a random delay and return the delay."""
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
