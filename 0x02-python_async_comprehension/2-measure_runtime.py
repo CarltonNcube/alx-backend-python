5cdab57e116b90b4ae86cecd03b1c23eb5c0e965
CarltonNcube <carltonncube@outlook.com>
2024-03-20 16:15:13 +0000

    Import async_comprehension from the previous file and write a measure_runtime coroutine that will execute async_comprehension

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """
-    Measures the runtime of executing async_comprehension four times in parallel.
+    Measures the runtime of executing async_comprehension 4 times in parallel.
 
     Returns:
         float: Total runtime in seconds.
