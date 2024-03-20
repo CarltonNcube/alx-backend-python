a1453886732d7ee4e875e2a4cc3bff0e28a47abb
CarltonNcube <carltonncube@outlook.com>
2024-03-20 16:25:02 +0000

    Defines a coroutine that generates random numbers asynchronously

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Defines a coroutine that generates random numbers asynchronously."""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronously generates random numbers between 0 and 10.
 
